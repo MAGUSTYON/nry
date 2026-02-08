bc824f1483110a50ec70d702e882003dc62c2f22
Tyon <82318775+MAGUSTYON@users.noreply.github.com>
2026-02-08 21:41:55 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-import { supabase } from "./supabaseClient.js";
+import { supabase } from "supabaseClient.js";
 
 /* ========= ELEMENTS ========= */
 const loginSection = document.getElementById("loginSection");
